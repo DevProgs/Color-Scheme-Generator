f9ab2215cb061a37b15ae341ed3b6d770d62ed4d
DevProgs <DevProgs1@gmail.com>
2023-03-28 01:54:50 -0500

    Test fetch to color api

--- main.js
+++ main.js
@@ -1,1 +1,5 @@
-
+fetch(
+  'https://www.thecolorapi.com/scheme?hex=0047AB&rgb=0,71,171&hsl=215,100%,34%&cmyk=100,58,0,33&mode=analogic&count=6'
+)
+  .then((res) => res.json())
+  .then((data) => console.log(data));
