fdf4d26f3eefc31edd092324558f53906a43d9e3
DevProgs <DevProgs1@gmail.com>
2023-03-29 03:43:03 -0500

    Button now calls api with selected input color

--- main.js
+++ main.js
@@ -1,33 +1,31 @@
 const schemeContainer = document.querySelector('.scheme-container')
-let testHTML = ''
+const btn = document.querySelector('.btn')
+const selectedColor = document.querySelector('.color-picker');
 
-// const color = document.querySelector('.color')
-// const color2 = document.querySelector('.color2')
-// const color3 = document.querySelector('.color3')
-// const color4 = document.querySelector('.color4')
-// const color5 = document.querySelector('.color5')
-// data.colors[0].hex.value;
-// fetch('https://www.thecolorapi.com/scheme?hex=0047AB&mode=analogic&count=5')
-//   .then((res) => res.json())
-//   .then(
-//     (data) => (color.style.backgroundColor = data.colors[0].hex.value,
-//     color2.style.backgroundColor = data.colors[1].hex.value,
-//     color3.style.backgroundColor = data.colors[2].hex.value,
-//     color4.style.backgroundColor = data.colors[3].hex.value,
-//     color5.style.backgroundColor = data.colors[4].hex.value
-//   ));
 
-fetch('https://www.thecolorapi.com/scheme?hex=0047AB&mode=analogic&count=5')
-  .then((res) => res.json())
-  .then((data) =>
-    data.colors.forEach(
-      (color) =>
-        (testHTML += `<div style="background: ${color.hex.value}"></div>`,
-        schemeContainer.innerHTML = testHTML
-      ),
-    )
-);
+const getColorScheme = () => {
+  let schemeHTML = '';
+  fetch(
+    `https://www.thecolorapi.com/scheme?hex=${selectedColor.value.substring(
+      1
+    )}&mode=analogic&count=5`
+  )
+    .then((res) => res.json())
+    .then((data) =>
+      data.colors.forEach(
+        (color) => (
+          (schemeHTML += `<div style="background: ${color.hex.value}"></div>`),
+          schemeContainer.innerHTML = schemeHTML
+        ),
+        schemeHTML = ''
+      )
+    );
+}
 
+btn.addEventListener('click', getColorScheme)
+// btn.addEventListener('click', getColorScheme)
+
+getColorScheme()
 
 // fetch('https://www.thecolorapi.com/scheme?hex=0047AB&mode=analogic&count=5')
 //   .then((res) => res.json())
