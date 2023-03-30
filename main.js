d3797a758455bc65f86ad6b2567e5867e2d0e18d
DevProgs <DevProgs1@gmail.com>
2023-03-30 03:09:30 -0500

    Proper hex values now show up below color column

--- main.js
+++ main.js
@@ -2,9 +2,10 @@ const schemeContainer = document.querySelector('.scheme-container');
 const btn = document.querySelector('.btn');
 const selectedColor = document.querySelector('.color-picker');
 const selectedMode = document.querySelector('.selector')
+const hexValueContainer = document.querySelector('.hex-value--container')
 
 const getColorScheme = () => {
-  console.log(selectedMode.value)
+  let hexHTML = ''
   let schemeHTML = '';
   fetch(
     `https://www.thecolorapi.com/scheme?hex=${selectedColor.value.substring(
@@ -15,7 +16,8 @@ const getColorScheme = () => {
     .then((data) =>
       data.colors.forEach(
         (color) => (
-          (schemeHTML += `<div style="background: ${color.hex.value}"></div>`),
+          (schemeHTML += `<div style="background: ${color.hex.value}"></div>`, hexHTML += `<div class="hex-value">${color.hex.value}</div>`),
+          (hexValueContainer.innerHTML = hexHTML),
           (schemeContainer.innerHTML = schemeHTML)
         ),
         (schemeHTML = '')
@@ -27,7 +29,3 @@ btn.addEventListener('click', getColorScheme);
 
 getColorScheme();
 
-
-// fetch('https://www.thecolorapi.com/scheme?hex=ff00ff&mode=analogic&count=5')
-//   .then((res) => res.json())
-//   .then((data) => console.log(data._links.schemes));
