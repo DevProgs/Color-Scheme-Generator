747597a0a87060d0bb5dc0c57ae87b0c8074af9d
DevProgs <DevProgs1@gmail.com>
2023-03-30 02:46:05 -0500

    Added Color API modes as select options

--- main.js
+++ main.js
@@ -1,8 +1,35 @@
 const schemeContainer = document.querySelector('.scheme-container');
 const btn = document.querySelector('.btn');
 const selectedColor = document.querySelector('.color-picker');
+const selectedMode = document.querySelector('.selector')
 
+
+// analogic: '/scheme?hex=FF00FF&mode=analogic&count=5';
+
+// "/scheme?hex=FF00FF&mode=analogic&count=5"
+// analogic-complement
+
+// "/scheme?hex=FF00FF&mode=analogic-complement&count=5"
+// complement
+
+// "/scheme?hex=FF00FF&mode=complement&count=5"
+// monochrome
+
+// "/scheme?hex=FF00FF&mode=monochrome&count=5"
+// monochrome-dark
+
+// "/scheme?hex=FF00FF&mode=monochrome-dark&count=5"
+// monochrome-light
+
+// "/scheme?hex=FF00FF&mode=monochrome-light&count=5"
+// quad
+
+// "/scheme?hex=FF00FF&mode=quad&count=5"
+// triad
+
+// "/scheme?hex=FF00FF&mode=triad&count=5"
 const getColorScheme = () => {
+  console.log(selectedMode.value)
   let schemeHTML = '';
   fetch(
     `https://www.thecolorapi.com/scheme?hex=${selectedColor.value.substring(
@@ -25,6 +52,7 @@ btn.addEventListener('click', getColorScheme);
 
 getColorScheme();
 
-fetch('https://www.thecolorapi.com/scheme?hex=0047AB&mode=analogic&count=5')
+// figure out how to get the scheme names into the input selector field
+fetch('https://www.thecolorapi.com/scheme?hex=ff00ff&mode=analogic&count=5')
   .then((res) => res.json())
   .then((data) => console.log(data));
