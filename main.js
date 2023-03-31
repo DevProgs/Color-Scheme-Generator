98ee2af41fc9020425b8362b91980772d9b649bd
DevProgs <DevProgs1@gmail.com>
2023-03-31 03:08:39 -0500

    Added feature to copy color to clipboard

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const selectedColor = document.querySelector('.color-picker');
 const selectedMode = document.querySelector('.selector')
 const hexValueContainer = document.querySelector('.hex-value--container')
 
+document.addEventListener('click', (e) => {
+  navigator.clipboard.writeText(e.target.dataset.color);
+})
+
 const getColorScheme = () => {
   let hexHTML = ''
   let schemeHTML = '';
@@ -16,16 +20,17 @@ const getColorScheme = () => {
     .then((data) =>
       data.colors.forEach(
         (color) => (
-          schemeHTML += `<div style="background: ${color.hex.value}"></div>`, 
+          schemeHTML += `<div data-color=${color.hex.value} style="background: ${color.hex.value}"></div>`, 
           hexHTML += `<div class="hex-value">${color.hex.value}</div>`,
           hexValueContainer.innerHTML = hexHTML,
           schemeContainer.innerHTML = schemeHTML
         ),
       )
     );
-};
+  };
+  
+  btn.addEventListener('click', getColorScheme);
 
-btn.addEventListener('click', getColorScheme);
+  getColorScheme();
 
-getColorScheme();
 
